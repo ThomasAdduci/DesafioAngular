718591e1a4fbae7afaddd69794d768ab5934f266
Thomas Adduci <thomaseadduci@gmail.com>
2022-08-11 22:04:40 -0300

    Limpie el proyecto de secciones no competentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { ToolbarComponent } from './toolbar/toolbar.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { ContentComponent } from './content/content.component';
-import { DirectivasComponent } from './components/directivas/directivas.component';
-import { DirectivaPersonalizadaDirective } from './directives/directiva-personalizada.directive';
-import { DirectivasDesafioComponent } from './Desafio2/directivas-desafio/directivas-desafio.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FormularioReactivoComponent } from './components/formulario-reactivo/formulario-reactivo.component';
 
@@ -16,10 +11,6 @@ import { FormularioReactivoComponent } from './components/formulario-reactivo/fo
     AppComponent,
     ToolbarComponent,
     NavbarComponent,
-    ContentComponent,
-    DirectivasComponent,
-    DirectivaPersonalizadaDirective,
-    DirectivasDesafioComponent,
     FormularioReactivoComponent
   ],
   imports: [
